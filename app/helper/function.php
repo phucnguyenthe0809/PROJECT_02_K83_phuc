1b872f8e557198b1d5dc0213fb8d6de8f866fd51
phamtungbg <phamtungbg95@gmail.com>
2019-11-19 20:46:14 +0700

    19/11

--- app/helper/function.php
+++ app/helper/function.php
@@ -9,3 +9,37 @@ function showErrors($errors,$name){
 }
 
 
+function getCate($arr,$parent,$tab){
+	foreach($arr as $value){
+
+		if($value['parent']==$parent){
+			echo '<option value="'.$value['id'].'">'.$tab.$value['name'].'</option>';
+
+		    getCate($arr,$value['id'],$tab.'--|');
+
+		}
+	}
+}
+
+function showCate($arr,$parent,$tab){
+	foreach($arr as $value){
+
+		if($value['parent']==$parent){
+			echo '<div class="item-menu"><span>'.$tab.$value['name'].'</span><div class="category-fix">';
+            echo '<a class="btn-category btn-primary" href="editcategory.html"><i class="fa fa-edit"></i></a>';
+            echo '<a class="btn-category btn-danger" href="#"><i class="fas fa-times"></i></i></a>';
+            echo '</div></div>';
+
+		    showCate($arr,$value['id'],$tab.'--|');
+
+		}
+	}
+}
+
+
+
+
+
+
+
+
